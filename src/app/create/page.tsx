14e9bd97f9d448facbcf4514952c7030a7033a36
Rick Das <rickd7587@gmail.com>
2025-11-28 13:34:33 +0530

    header & page modified

---
+++ src/app/create/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Create user | User CRUD",
+	description: "Create user page of User CRUD App",
+};
+const page = () => {
+	return (
+		<>
+			<div className=""></div>
+		</>
+	);
+};
+
+export default page;
